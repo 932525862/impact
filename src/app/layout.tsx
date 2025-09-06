0d633553fcc724859279acd4bfd7a7f9f3b03eff
Muhammadyusuf <mmuhammadyusuf302@gmail.com>
2025-09-06 16:39:31 +0500

    malumot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "IMPACT FINANCE - Mikrokredit Xizmatlari",
   description:
-  "Biznes egalari, yuridik shaxslar va jismoniy shaxslarga mikrokredit va qarz berish xizmatlari. Tezkor va ishonchli moliyaviy yordam.",
-  
-  
+    "Biznes egalari, yuridik shaxslar va jismoniy shaxslarga mikrokredit va qarz berish xizmatlari. Tezkor va ishonchli moliyaviy yordam.",
   keywords: [
     "mikrokredit",
     "qarz olish",
@@ -34,7 +32,51 @@ export const metadata: Metadata = {
     "tezkor kredit",
     "shaxsiy kredit",
     "korporativ kredit",
+    "www.impactfinance.uz",
+    "impactfinance.uz"
   ],
+  metadataBase: new URL("https://www.impactfinance.uz/"),
+  openGraph: {
+    title: "IMPACT FINANCE - Mikrokredit Xizmatlari",
+    description:
+      "Biznes egalari, yuridik shaxslar va jismoniy shaxslarga mikrokredit va qarz berish xizmatlari. Tezkor va ishonchli moliyaviy yordam.",
+    url: "https://www.impactfinance.uz/",
+    siteName: "IMPACT FINANCE",
+    images: [
+      {
+        url: "https://www.impactfinance.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo2.202c46ad.png&w=256&q=75",
+        width: 1200,
+        height: 630,
+        alt: "IMPACT FINANCE - Mikrokredit Xizmatlari",
+      },
+    ],
+    locale: "uz_UZ",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "IMPACT FINANCE - Mikrokredit Xizmatlari",
+    description:
+      "Biznes egalari, yuridik shaxslar va jismoniy shaxslarga mikrokredit va qarz berish xizmatlari. Tezkor va ishonchli moliyaviy yordam.",
+    images: ["https://www.impactfinance.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo2.202c46ad.png&w=256&q=75"],
+    site: "@impactfinance", // agar Twitter akkauntingiz bo'lsa
+  },
+  icons: {
+    icon: "/logo2.png",
+    shortcut: "/logo2.png",
+    apple: "/apple-touch-icon.png",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 }
 
 export default function RootLayout({
