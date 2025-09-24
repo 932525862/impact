1c11b700c7b09650b639e286f3cdcb1655914d42
Muhammadyusuf <mmuhammadyusuf302@gmail.com>
2025-09-24 13:49:57 +0500

    matin

--- src/commponents/teamSection.tsx
+++ src/commponents/teamSection.tsx
@@ -2,28 +2,31 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import { useTranslation } from "react-i18next";
 
 export default function WeAreLookingFor() {
+  const { t } = useTranslation();
+
   const items = [
     {
       img: "/bank.jpeg",
-      title: "Avazbekov Maqsadbek",
-      text: "Biz birgalikda ishlaymiz, bir-birimizi qo‘llab-quvvatlaymiz va umumiy maqsadlar sari intilamiz. Birgalikda kuchliroqmiz!",
+      title: t("items.jaloliddin.title"),
+      text: t("items.jaloliddin.text"),
     },
     {
       img: "/bank.jpeg",
-      title: "Avazbekov Maqsadbek",
-      text: "Biz dadil qarorlar va nostandart g‘oyalarni qadrlaymiz. Har bir xodim kompaniya rivojiga hissa qo‘shishi mumkin",
+      title: t("items.jamoliddin.title"),
+      text: t("items.jamoliddin.text"),
     },
     {
       img: "/bank.jpeg",
-      title:"Avazbekov Maqsadbek",
-      text: "Biz doimo rivojlanmoqdamiz va yangi narsalarni o‘rganamiz. O‘zimizga va imkoniyatlarimizga ishonamiz",
+      title: t("items.mirzo.title"),
+      text: t("items.mirzo.text"),
     },
     {
       img: "/bank.jpeg",
-      title: "Avazbekov Maqsadbek",
-      text: "Bevosita muloqot, halollik va ishonch jamoamizning asosidir. Biz har qanday g‘oyani tinglashga, eshitishga va muhokama qilishga tayyormiz.",
+      title: t("items.mirgulom.title"),
+      text: t("items.mirgulom.text"),
     },
   ];
 
@@ -32,7 +35,7 @@ export default function WeAreLookingFor() {
       <div className="max-w-7xl mx-auto px-4">
         {/* Title */}
         <h2 className="text-3xl md:text-4xl text-[#004526] font-bold text-center mb-12">
-        Boshqaruv va direktorlar kengashi
+          {t("sectionTitle")}
         </h2>
 
         {/* Desktop Grid */}
@@ -42,7 +45,7 @@ export default function WeAreLookingFor() {
               key={idx}
               className="bg-white rounded-lg shadow-sm border border-gray-100 overflow-hidden"
             >
-              <div className="relative   w-full h-68">
+              <div className="relative w-full h-68">
                 <Image
                   src={item.img}
                   alt={item.title}
@@ -51,7 +54,9 @@ export default function WeAreLookingFor() {
                 />
               </div>
               <div className="p-5">
-                <h3 className="font-semibold text-lg mb-2 text-[#004526]">{item.title}</h3>
+                <h3 className="font-semibold text-lg mb-2 text-[#004526]">
+                  {item.title}
+                </h3>
                 <p className="text-gray-600 text-sm leading-relaxed">
                   {item.text}
                 </p>
@@ -79,7 +84,7 @@ export default function WeAreLookingFor() {
                     />
                   </div>
                   <div className="p-5">
-                    <h3 className="font-semibold text-lg mb-2">{item.title}</h3>
+                    <h3 className="font-semibold text-[#000000] text-lg mb-2">{item.title}</h3>
                     <p className="text-gray-600 text-sm leading-relaxed">
                       {item.text}
                     </p>
